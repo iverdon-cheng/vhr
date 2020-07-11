ec5f7b999598ac58dcf66cd4df5d0779bb4cb83c
iverdon-cheng <294236220@qq.com>
2020-07-11 15:32:41 +0800

    vhr

---
+++ src/main/java/cn/iverdon/vhrlixi/VhrLixiApplication.java
@@ -0,0 +1,13 @@
+package cn.iverdon.vhrlixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrLixiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrLixiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/iverdon/vhrlixi/VhrLixiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.iverdon.vhrlixi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrLixiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
