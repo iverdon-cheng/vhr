cb87f320016012faf8b56b430ba0a7c922e2e640
iverdon-cheng <294236220@qq.com>
2020-07-15 10:30:42 +0800

    vhr 07.14

---
+++ src/main/java/cn/iverdon/vhrlixi/service/MenuService.java
@@ -0,0 +1,24 @@
+package cn.iverdon.vhrlixi.service;
+
+import cn.iverdon.vhrlixi.mapper.MenuMapper;
+import cn.iverdon.vhrlixi.model.Hr;
+import cn.iverdon.vhrlixi.model.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author iverdon
+ * @date 2020/7/14 4:27 下午
+ */
+@Service
+public class MenuService {
+    @Autowired
+    MenuMapper menuMapper;
+
+    public List<Menu> getMenusByHrId() {
+        return menuMapper.getMenusByHrId(((Hr)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+    }
+}
